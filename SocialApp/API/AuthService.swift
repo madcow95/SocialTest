67d33766e990a6fde2e5e94f1149a4d9652fa3eb
MadCow <lobasketve@naver.com>
2023-12-14 23:17:39 +0900

    Feat. 회원가입 기능 추가
    1. Auth API에 회원가입 기능 추가
        1-1. 최초에 회원가입 페이지에서 입력한 정보로 Authentication에 User를 생성한다.
        1-2. 생성된 User의 UID를 가져와 입력한 정보와 함께 Firebase Database에 저장한다.

--- SocialApp/API/AuthService.swift
+++ SocialApp/API/AuthService.swift
@@ -8,10 +8,37 @@
 import UIKit
 import Firebase
 
-struct AuthService {
+struct AuthCredentials {
     let email: String
     let password: String
     let fullName: String
     let nickName: String
     let profileImage: UIImage
+}
+
+struct AuthService {
+    static func registerUser(with credential: AuthCredentials, completion: @escaping(Error) -> Void) {
+        ImageUploader.uploadImage(image: credential.profileImage) { imageUrl in
+            Auth.auth().createUser(withEmail: credential.email, password: credential.password) { createResult, error in
+                if let error = error {
+                    print("DEBUG: createUser Error: \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let uid = createResult?.user.uid else { return }
+                
+                var data: [String: Any] = ["email": credential.email,
+                                           "fullName": credential.fullName,
+                                           "nickName": credential.nickName,
+                                           "profileImage": credential.profileImage]
+                
+                Firestore.firestore().collection("users").document(uid).setData(data) { error in
+                    if let error = error {
+                        print("ERROR DEBUG: save data error >> \(error.localizedDescription)")
+                        return
+                    }
+                }
+            }
+        }
+    }
 }
